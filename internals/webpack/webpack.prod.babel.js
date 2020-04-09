b3a5877f1752d10a7b8953b2e3d8956bdd81b9ef
Chandu's <chandu.prince951@gmail.com>
2020-04-09 00:39:57 -0700

    Favicon webpack deleted :

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -116,24 +116,24 @@ module.exports = require('./webpack.base.babel')({
     }),
 
     new WebpackPwaManifest({
-      name: 'React Boilerplate',
+      name: 'IMDB Movies App',
       short_name: 'React BP',
       description: 'My React Boilerplate-based project!',
       background_color: '#fafafa',
       theme_color: '#b1624d',
       inject: true,
       ios: true,
-      icons: [
-        {
-          src: path.resolve('app/images/icon-512x512.png'),
-          sizes: [72, 96, 128, 144, 192, 384, 512],
-        },
-        {
-          src: path.resolve('app/images/icon-512x512.png'),
-          sizes: [120, 152, 167, 180],
-          ios: true,
-        },
-      ],
+      // icons: [
+      //   {
+      //     src: path.resolve('app/images/icon-512x512.png'),
+      //     sizes: [72, 96, 128, 144, 192, 384, 512],
+      //   },
+      //   {
+      //     src: path.resolve('app/images/icon-512x512.png'),
+      //     sizes: [120, 152, 167, 180],
+      //     ios: true,
+      //   },
+      // ],
     }),
 
     new HashedModuleIdsPlugin({
